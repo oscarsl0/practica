0dd65f4eda8d397ff81487f171a1d1f3d6a31677
“ <>
2025-09-05 11:53:47 -0600

    commit en git

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR Hola soy yo de nuevo Josue Nolasco
+          Edit <code>src/App.jsx prueba</code> and save to test HMR Hola soy yo de nuevo Josue Nolasco
         </p>
       </div>
       <p className="read-the-docs">
